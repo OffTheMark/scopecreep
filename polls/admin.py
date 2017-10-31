8fa402f9673c3d0324cf314326b511211586e1d0
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2017-10-31 16:02:30 -0400

    Add admin configuration for app data models

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import SuggestionBox, Suggestion, Comment, Vote
+
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 0
+
+
+class VoteInline(admin.StackedInline):
+    model = Vote
+    extra = 0
+
+
+class SuggestionBoxAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "date_created"]
+    list_filter = ["date_created"]
+    search_fields = ["name", "description"]
+
+
+class SuggestionAdmin(admin.ModelAdmin):
+    inlines = [CommentInline, VoteInline]
+    list_display = ["name", "description", "date_created"]
+    list_filter = ["date_created"]
+    search_fields = ["name", "description"]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["text", "date_created"]
+    list_filter = ["date_created"]
+    search_fields = ["text"]
+
+
+admin.site.register(SuggestionBox, SuggestionBoxAdmin)
+admin.site.register(Suggestion, SuggestionAdmin)
+admin.site.register(Comment, CommentAdmin)
