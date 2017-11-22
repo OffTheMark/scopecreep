e1a3b81233ff5f8151364256fb4a96905929f5f6
Marc-Antoine Malépart <mantoine.malepart@gmail.com>
2017-11-22 11:32:11 -0500

    Refactor URLs to group URLs with common pattern prefixes

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,26 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 from . import views
 
 app_name = 'polls'
+
+topic_patterns = [
+    url(r'^$', views.TopicView.as_view(), name='topic'),
+    url(r'^edit/$', views.EditTopicView.as_view(), name='topic_edit'),
+    url(r'^delete/$', views.delete_topic, name='topic_delete'),
+]
+
+suggestion_patterns = [
+    url(r'^$', views.SuggestionView.as_view(), name='suggestion'),
+]
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^signin/', views.SigninView.as_view(), name='signin'),
     url(r'^logout/$', views.signout, name='signout'),
     url(r'^signup/$', views.SignupView.as_view(), name='signup'),
     url(r'^topics/$', views.TopicsView.as_view(), name='topics'),
-    url(r'^topic/(?P<topic_id>[0-9]+)/$', views.TopicView.as_view(), name='topic'),
-    url(r'^topic/(?P<topic_id>[0-9]+)/edit/$', views.EditTopicView.as_view(), name='topic_edit'),
-    url(r'^topic/(?P<topic_id>[0-9]+)/delete/$', views.delete_topic, name='topic_delete'),
-    url(r'^suggestion/(?P<suggestion_id>[0-9]+)/$', views.SuggestionView.as_view(), name='suggestion'),
+    url(r'^topic/(?P<topic_id>[0-9]+)/', include(topic_patterns)),
+    url(r'^suggestion/(?P<suggestion_id>[0-9]+)/', include(suggestion_patterns)),
+
 ]
